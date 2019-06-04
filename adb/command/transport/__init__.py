35177935348c040ec70e670a33c0ff947dc308cb
visu4l <ch.visu4l@gmail.com>
2019-06-04 13:48:30 +0900

    update shell decode example : shell result = bytearray(b'\xec\x98\x81\n(\xed\x95\xad\xea\xb3\n06-04 13') when the abobe results are included. modified to ignore unknown encoding errors

--- adb/command/transport/__init__.py
+++ adb/command/transport/__init__.py
@@ -27,7 +27,7 @@ class Transport(Command):
         else:
             result = conn.read_all()
             conn.close()
-            return result.decode('utf-8')
+            return result.decode(errors='ignore')
 
     def sync(self):
         conn = self.create_connection()
